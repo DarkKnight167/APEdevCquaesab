a107a9f02bd6f0b3580063797438a3818ef04a36
DarkKnight167 <135136389+DarkKnight167@users.noreply.github.com>
2023-09-13 11:19:46 -0300

    Add files via upload

--- Aulas de APE/main2.c
+++ Aulas de APE/main2.c
@@ -7,11 +7,51 @@ int main(){
 	%lf
 	*/
 	
-	printf("Peso: %.2lf \n", 54.2);
-	printf("Altura: %.2lf\n", 1.68);
-	printf("Idade: %d\n",18);
-	printf("Salario: %.3f\n",9.545);
-
 	
+//	int idade;
+//	float peso;
+//	float altura;
+//	float salario;
+//	char nome[80];
+//	 
+//	 
+//	 
+//	printf("Insira seu nome:\n");
+//	scanf("%s" , nome);
+//	printf("Insira seu peso:\n");
+//	scanf("%f" , &peso );
+//	printf("Insira sua altura:\n");
+//	scanf("%f" , &altura);
+//	printf("Insira sua idade:\n");
+//	scanf("%i" , &idade );
+//	printf("Insira seu salário:\n");
+//	scanf("%f", &salario );
+//
+//	printf("&s, %f, %f, %i %f", nome ,&peso, &idade, &salario, &altura);
+
+
+
+
+
+//float n1,n2 , r; 
+//
+//printf("Digite seu salário: \n");
+//scanf("%f", &n1);
+//printf("Digite o salario minimo em seu país: \n");
+//scanf("%f", &n2);
+//
+//r=n1/n2;
+//
+//printf("resultado: %f", r);
+
+
+
+
+
+
+
+
+
+
 	
 }
