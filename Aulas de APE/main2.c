5ffe054baf99c7af7a85c0ad3128b299abbc9faf
DarkKnight167 <135136389+DarkKnight167@users.noreply.github.com>
2023-09-13 09:54:34 -0300

    Add files via upload

---
+++ Aulas de APE/main2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){	
+	/*
+	%f 
+	%d 
+	%lf
+	*/
+	
+	printf("Peso: %.2lf \n", 54.2);
+	printf("Altura: %.2lf\n", 1.68);
+	printf("Idade: %d\n",18);
+	printf("Salario: %.3f\n",9.545);
+
+	
+	
+}
